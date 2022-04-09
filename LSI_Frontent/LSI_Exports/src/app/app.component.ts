b2498ea58bbea30508b6785cc6e8eb79978497d1
marcsien <marcsien@gmail.com>
2022-04-09 12:42:48 +0200

    Some changes in frontend. Frontent not yet finished.

--- LSI_Frontent/LSI_Exports/src/app/app.component.ts
+++ LSI_Frontent/LSI_Exports/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //template: '<app-home></app-home>',
 })
 export class AppComponent {
   title = 'LSI_Exports';
