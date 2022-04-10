dc1f37be71f52a03fe286a5d8439b75a587a621e
marcsien <marcsien@gmail.com>
2022-04-10 08:36:53 +0200

    Add input components from material framework.

--- LSI_Frontent/LSI_Exports/src/app/app.component.ts
+++ LSI_Frontent/LSI_Exports/src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 
+interface Local {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //template: '<app-home></app-home>',
 })
 export class AppComponent {
   title = 'LSI_Exports';
+  selectedLocal: string = 'Local1';
+
+  locals: Local[] = [
+    {value: 'Local1', viewValue: 'Local1'},
+    {value: 'Local2', viewValue: 'Local2'},
+    {value: 'Local3', viewValue: 'Local3'},
+    {value: 'Local4', viewValue: 'Local4'},
+    {value: 'Local5', viewValue: 'Local5'},
+  ];
 }
--- LSI_Frontent/LSI_Exports/src/app/app.module.ts
+++ LSI_Frontent/LSI_Exports/src/app/app.module.ts
@@ -3,18 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatSelectModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatNativeDateModule
+  ]
 })
 export class AppModule { }
